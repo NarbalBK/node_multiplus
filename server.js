5297ccb312bfc8f72a5564957cdb40172131817d
periclesnarbal <periclesnarbal@gmail.com>
2020-02-09 18:05:51 -0300

    remove player when disconnect

--- server.js
+++ server.js
@@ -11,13 +11,27 @@ app.use(express.static('public'))
 
 const game = createGame()
 
+game.subscribe((command) => {
+    console.log(`> Emitting ${command.type}`)
+    sockets.emit(command.type, command)
+})
+    
+
 console.log(game.state)
 
 sockets.on('connection', (socket) => {
     const playerId = socket.id
-    console.log(`> Player connected on Client with id ${playerId}`)
+    console.log(`> Player connected ${playerId}`)
+
+    game.addPlayer({playerId:playerId})
+    //console.log(game.state)
 
     socket.emit('setup', game.state)
+
+    socket.on('disconnect', () =>{
+        game.removePlayer({playerId: playerId})
+        console.log(`player disconnected: ${playerId}`)
+    })
 })
 
 server.listen(3000, () => {
